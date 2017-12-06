2152f5155914a3ac0d761eac63e8be6ce3ca1329
hoai-nam20132674 <namnguyen20132674@gmail.com>
2017-12-06 10:08:24 +0700

    add js

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -39,28 +39,22 @@
         <script type="text/javascript">
                 $(document).ready(function () {
 
+                    var request= $("#search_text").val()
                     
-                        
                     $("#search_text").keypress(function (e) {
                         if (e.keyCode == 13){
                             e.preventDefault();
                             var request= $("#search_text").val()
-                                // $('#pagination-demo').twbsPagination({
-                                //     totalPages: 35,
-                                //     visiblePages: 7,
-                                //     onPageClick: function (event, page) {
-                                //         $('#page-content').text('Page ' + page);
-                                //     }
-                                // });
-                            
+                                
                                 $.ajax({ //create an ajax request to load_page.php
                                     type: "GET",
                                     url: "http://localhost:9099/search?query="+request+"&page=1",
                                     dataType: "json", //expect html to be returned                
                                     success: function (data) {
                                         
+                                        
+                                        
                                         var quantity=data.quantity;
-                                        var page_total = Math.floor(quantity/10)+1;
                                         var html_str = '<div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><a style="color: #545454; margin-top: -10px;font-family: Sans-serif;">Khoảng '+quantity+' kết quả (0,01 giây)</a></div></div><br><div class="article"><div class="row"><div class="col-sm-1"></div>';
                                         for(var i=0; i< data.data.length; i++){
 
@@ -72,16 +66,47 @@
                                             html_str += '<div class=" test col-sm-11"><a href="'+url+'" target="_blank" ><h5 class="title" style="color: #1a0dab; font-size: 19px; margin-top: 1px;font-family: Sans-serif;">' + title + '</h5></a></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><h6 class="Url" style="color: #006621; margin-top: -13px;font-family: Sans-serif;">' + url  + '</h6></div></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><a class="info" style="color: #545454; margin-top: -10px;font-family: Sans-serif;">'+snippet+'</a></div></div><br><div class="article"><div class="row"><div class="col-sm-1"></div>';
                                             
                                         }
+                                        $('#display').html(html_str);
+                                        
+                                        var page_total = Math.floor(quantity/10)+1;
                                         $("#pagination-demo").twbsPagination({
+
 
                                             totalPages: page_total,
                                             visiblePages: 7,
                                             onPageClick: function (event, page) {
                                                 
+                                                var request= $("#search_text").val()
+                                                // alert(request);
+                                                $.ajax({ //create an ajax request to load_page.php
+                                                    type: "GET",
+                                                    url: "http://localhost:9099/search?query="+request+"&page="+page+"",
+                                                    dataType: "json", //expect html to be returned                
+                                                    success: function (data) {
+                                                        
+                                                        var quantity=data.quantity;
+                                                        // var page_total = Math.floor(quantity/10)+1;
+                                                        var html_str = '<div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><a style="color: #545454; margin-top: -10px;font-family: Sans-serif;">Khoảng '+quantity+' kết quả (0,01 giây)</a></div></div><br><div class="article"><div class="row"><div class="col-sm-1"></div>';
+                                                        for(var i=0; i< data.data.length; i++){
+
+                                                            var url= data.data[i].url;
+                                                            var title=data.data[i].title;
+                                                            var snippet=data.data[i].snippet;
+                                                            
+
+                                                            html_str += '<div class=" test col-sm-11"><a href="'+url+'" target="_blank" ><h5 class="title" style="color: #1a0dab; font-size: 19px; margin-top: 1px;font-family: Sans-serif;">' + title + '</h5></a></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><h6 class="Url" style="color: #006621; margin-top: -13px;font-family: Sans-serif;">' + url  + '</h6></div></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><a class="info" style="color: #545454; margin-top: -10px;font-family: Sans-serif;">'+snippet+'</a></div></div><br><div class="article"><div class="row"><div class="col-sm-1"></div>';
+                                                            
+                                                        }
+                                                    $('#display').html(html_str);
+
+                                                    }
+                                                });
+                                            
+                                                
                                             }
                                         });
 
-                                        $('#display').html(html_str);
+                                        // $('#display').html(html_str);
                                         console.log(data);
                                        
                                     }
@@ -92,6 +117,54 @@
                          //end if
                     
                     });
+
+                    $.ajax({ //create an ajax request to load_page.php
+                        type: "GET",
+                        url: "http://localhost:9099/search?query="+request+"&page=1",
+                        dataType: "json", //expect html to be returned                
+                        success: function (data) {
+
+
+                            // $("#pagination-demo").twbsPagination({
+
+                            //     totalPages: data.quantity,
+                            //     visiblePages: 7,
+                            //     onPageClick: function (event, page) {
+                                    
+                            //         var request= $("#search_text").val()
+
+                            //         alert(data.data.length);
+                            //     }
+                            // });
+                            
+                        }
+                    });
+                    
+                    $.ajax({ //create an ajax request to load_page.php
+                        type: "GET",
+                        url: "http://localhost:9099/search?query="+request+"&page=1",
+                        dataType: "json", //expect html to be returned                
+                        success: function (data) {
+                            
+                            var quantity=data.quantity;
+                            // var page_total = Math.floor(quantity/10)+1;
+                            var html_str = '<div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><a style="color: #545454; margin-top: -10px;font-family: Sans-serif;">Khoảng '+quantity+' kết quả (0,01 giây)</a></div></div><br><div class="article"><div class="row"><div class="col-sm-1"></div>';
+                            for(var i=0; i< data.data.length; i++){
+
+                                var url= data.data[i].url;
+                                var title=data.data[i].title;
+                                var snippet=data.data[i].snippet;
+                                
+
+                                html_str += '<div class=" test col-sm-11"><a href="'+url+'" target="_blank" ><h5 class="title" style="color: #1a0dab; font-size: 19px; margin-top: 1px;font-family: Sans-serif;">' + title + '</h5></a></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><h6 class="Url" style="color: #006621; margin-top: -13px;font-family: Sans-serif;">' + url  + '</h6></div></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><a class="info" style="color: #545454; margin-top: -10px;font-family: Sans-serif;">'+snippet+'</a></div></div><br><div class="article"><div class="row"><div class="col-sm-1"></div>';
+                                
+                            }
+                            $('#display').html(html_str);
+                            // console.log(data);
+                        }
+                    });
+                    
+
                 });
         </script>
     </head>
