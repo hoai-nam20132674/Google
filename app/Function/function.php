8bedc90b7c5da330b41c593e28c6d3db07f79d8c
hoai-nam20132674 <namnguyen20132674@gmail.com>
2017-12-08 17:45:17 +0700

    text_limit.js

--- app/Function/function.php
+++ app/Function/function.php
@@ -4,16 +4,18 @@
 			$str_s = "";
 			if(stripos($str," ")){
 				$ex_str = explode(" ",$str);
-			if(count($ex_str)>$limit){
-				for($i=0;$i<$limit;$i++){
-				$str_s.=$ex_str[$i]." ";
+				if(count($ex_str)>$limit){
+					for($i=0;$i<$limit;$i++){
+						$str_s.=$ex_str[$i]." ";
+					}
+					$str_s = $str_s."...";
+					return $str_s;
 				}
-				$str_s = $str_s."...";
-			return $str_s;
-			}else{
-				return $str;
+				else{
+					return $str;
 				}
-			}else{
+			}
+			else{
 				return $str;
 			}
 		}
--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
 
     var request= $("#search_text").val()
-    
+    var start = new Date();
     $("#search_text").keypress(function (e) {
         if (e.keyCode == 13){
             e.preventDefault();
-            $("#pagination-demo div").html("");
             var request= $("#search_text").val();
                 
             $.ajax({ //create an ajax request to load_page.php
@@ -14,8 +13,6 @@ $(document).ready(function () {
                 dataType: "json", //expect html to be returned                
                 success: function (data) {
                     
-                    
-                    
                     var quantity=data.quantity;
                     var page_total = Math.floor(quantity/10)+1;
                     var html_str = '<div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><a style="color: #545454; margin-top: -10px;font-family: Sans-serif;">Khoảng '+quantity+' kết quả (0,01 giây)</a></div></div><br><div class="article"><div class="row"><div class="col-sm-1"></div>';
@@ -23,14 +20,24 @@ $(document).ready(function () {
 
 
                         var url= data.data[i].url;
-                        // if(url.length>10){
-                        //     url = url.substring(0, 10);
-                        // }
-                        var title=data.data[i].title;
-                        var snippet=data.data[i].snippet;
-                        
 
-                        html_str += '<div class=" test col-sm-6"><a href="'+url+'" target="_blank" ><h5 class="title" style="color: #1a0dab; font-size: 19px; margin-top: 1px;font-family: Sans-serif;">' + title.substring(0, 60) + ' ...</h5></a></div><div class="col-sm-5"></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-6"><h6 class="Url" style="color: #006621; margin-top: -13px;font-family: Sans-serif;">' + url.substring(0,80)  + '...</h6></div><div class="col-sm-5"></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><a class="info" style="color: #545454; margin-top: -10px;font-family: Sans-serif;">'+snippet+'</a></div></div><br><div class="article"><div class="row"><div class="col-sm-1"></div>';
+                        var title=data.data[i].title;
+                        var snippet="";
+                        var snippets=data.data[i].snippet;
+                        var array = snippets.split(" ");
+                        if(array.length>50){
+                            
+                            for(var a=0;a<=50;a++){
+                                snippet=snippet.concat(array[a]).concat(" ");
+
+                            }
+                            // snippet=snippet.concat("...");
+                        }
+                        else{
+                            snippet=snippets;
+                        }
+
+                        html_str += '<div class=" test col-sm-11"><a href="'+url+'" target="_blank" ><h5 class="title" style="color: #1a0dab; font-size: 19px; margin-top: 1px;font-family: Sans-serif;">' + title + '</h5></a></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><h6 class="Url" style="color: #006621; margin-top: -13px;font-family: Sans-serif;">' + url+ '</h6></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><a class="info" style="color: #545454; margin-top: -10px;font-family: Sans-serif;">'+snippet+'</a></div></div><br><div class="article"><div class="row"><div class="col-sm-1"></div>';
                         
                     }
                     $('#display').html(html_str);
@@ -49,18 +56,33 @@ $(document).ready(function () {
                                     
                                     var quantity=data.quantity;
                                     // var page_total = Math.floor(quantity/10)+1;
-                                    var html_str = '<div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><a style="color: #545454; margin-top: -10px;font-family: Sans-serif;">Khoảng '+quantity+' kết quả (0,01 giây)</a></div></div><br><div class="article"><div class="row"><div class="col-sm-1"></div>';
+                                    var html_str2 = '<div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><a style="color: #545454; margin-top: -10px;font-family: Sans-serif;">Khoảng '+quantity+' kết quả (0,01 giây)</a></div></div><br><div class="article"><div class="row"><div class="col-sm-1"></div>';
                                     for(var i=0; i< data.data.length; i++){
 
                                         var url= data.data[i].url;
                                         var title=data.data[i].title;
-                                        var snippet=data.data[i].snippet;
+                                        var snippet="";
+                                        var snippets=data.data[i].snippet;
+                                        var array = snippets.split(" ");
+                                        if(array.length>50){
+                                            
+                                            for(var a=0;a<=50;a++){
+                                                
+                                                snippet=snippet.concat(array[a]).concat(" ");
+ 
+                                            }
+                                            // snippet=snippet.concat("...");
+                                        }
+                                        else{
+                                            snippet=snippets;
+                                        }
+
                                         
 
-                                        html_str += '<div class=" test col-sm-6"><a href="'+url+'" target="_blank" ><h5 class="title" style="color: #1a0dab; font-size: 19px; margin-top: 1px;font-family: Sans-serif;">' + title.substring(0,60) + ' ...</h5></a></div><div class="col-sm-5"></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-6"><h6 class="Url" style="color: #006621; margin-top: -13px;font-family: Sans-serif;">' + url.substring(0,80)  + '...</h6></div><div class="col-sm-5"></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><a class="info" style="color: #545454; margin-top: -10px;font-family: Sans-serif;">'+snippet+'</a></div></div><br><div class="article"><div class="row"><div class="col-sm-1"></div>';
+                                        html_str2 += '<div class=" test col-sm-11"><a href="'+url+'" target="_blank" ><h5 class="title" style="color: #1a0dab; font-size: 19px; margin-top: 1px;font-family: Sans-serif;">' + title + '</h5></a></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><h6 class="Url" style="color: #006621; margin-top: -13px;font-family: Sans-serif;">' + url+ '</h6></div></div><div class="row"><div class="col-sm-1"></div><div class="col-sm-11"><a class="info" style="color: #545454; margin-top: -10px;font-family: Sans-serif;">'+snippet+'</a></div></div><br><div class="article"><div class="row"><div class="col-sm-1"></div>';
                                         
                                     }
-                                $('#display').html(html_str);
+                                $('#display').html(html_str2);
 
                                 }
                             });
